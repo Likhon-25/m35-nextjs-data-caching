b4a210993c005656bc1b80736550223737f7ac38
Likhon-25 <likhonmondol327@gmail.com>
2026-09-18 20:08:12 +0600

    Suspense and Streaming with Data Fetching

--- src/app/books/[bookId]/page.jsx
+++ src/app/books/[bookId]/page.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const generateStaticParams = async () => {
+export const generateStaticParams = async () => {
   const res = await fetch("http://localhost:5000/books");
   const books = await res.json();
 
-  return
+  return books.map(book => ({bookId: book.id}))
 };
 const BookDetailPage = async ({ params }) => {
   const { bookId } = await params;
